ff139cc2ce95e27d0b6d352168944f2e1062940f
Gavin <gavin42333@hotmail.com>
2018-04-03 22:22:33 +0800

    添加命令 使模块添加通过命令拷贝模般代码

---
+++ build/module.js
@@ -0,0 +1,41 @@
+const readlineSync = require('readline-sync')
+const fs = require('fs')
+const path = require('path')
+
+let moduleName = readlineSync.question('your module name: ')
+
+console.log('your module name is ' + moduleName)
+
+console.error(fs.existsSync(path.resolve(__dirname, '../src/modules', moduleName)))
+
+const modulePath = path.resolve(__dirname, '../src/modules', moduleName)
+const templatePath = path.resolve(__dirname, '../template')
+
+if (fs.existsSync(modulePath)) {
+  console.error(`${moduleName} module exists`)
+  process.exit()
+}
+
+fs.mkdirSync(modulePath)
+fs.mkdirSync(path.join(modulePath, 'router'))
+fs.mkdirSync(path.join(modulePath, 'views'))
+fs.mkdirSync(path.join(modulePath, 'views/home'))
+
+// 复制模板中的文件到目标文件夹
+const sourceFile = [
+  'main.js',
+  'App.vue',
+  'router/index.js',
+  'views/home/Index.vue'
+]
+const copy = (source) => {
+  for (let sitem of source) {
+    let fileText = fs.readFileSync(path.join(templatePath, sitem))
+
+    // todo: 对读取的内容进行操作
+
+    fs.writeFileSync(path.join(modulePath, sitem), fileText)
+  }
+}
+
+copy(sourceFile)
