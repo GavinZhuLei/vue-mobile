e7498a696ec799801e4a5f675ddd2d6fb9e0a552
Gavin <gavin42333@hotmail.com>
2018-04-08 10:23:56 +0800

    删除多余注释

--- build/entry.js
+++ build/entry.js
@@ -4,13 +4,9 @@ const fs = require('fs')
 
 const moduleDir = path.resolve(__dirname, '../src/modules')
 
-console.log('log dir')
-console.log(moduleDir)
-
 let entryObj = {}
 
 let moduleItems = fs.readdirSync(moduleDir)
-console.log(moduleItems)
 
 moduleItems.forEach(item => {
   entryObj[`${item}`] = `./src/modules/${item}/main.js`
